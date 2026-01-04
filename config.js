750ec054a6ebcd5bcaed3df2e455478bc3331f10
MyHanh10 <Hanhltm10@fe.edu.vn>
2026-01-04 23:31:24 +0700

    Upload config.js for Vercel

---
+++ config.js
@@ -0,0 +1,9 @@
+// File này chứa thông tin mật, KHÔNG đưa lên GitHub
+export const firebaseConfig = {
+    apiKey: "AIzaSyCp1sj2Esoyguc8BBFS8u7nQJhAkOusKSE",
+    authDomain: "all-ncc-hha.firebaseapp.com",
+    projectId: "all-ncc-hha",
+    storageBucket: "all-ncc-hha.firebasestorage.app",
+    messagingSenderId: "722766356262",
+    appId: "1:722766356262:web:b6e999ea077623e2a02be7",
+};
