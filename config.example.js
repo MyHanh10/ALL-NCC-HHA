cefda41ad397c354cff5a9539ca0fc754b0c3af9
MyHanh10 <Hanhltm10@fe.edu.vn>
2026-01-04 22:32:00 +0700

    ne

---
+++ config.example.js
@@ -0,0 +1,9 @@
+// Đây là file mẫu. Hãy đổi tên thành config.js và điền thông tin thật vào
+export const firebaseConfig = {
+    apiKey: "YOUR_API_KEY",
+    authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
+    projectId: "YOUR_PROJECT_ID",
+    storageBucket: "YOUR_PROJECT_ID.appspot.com",
+    messagingSenderId: "YOUR_SENDER_ID",
+    appId: "YOUR_APP_ID"
+};
